462947f2c5d3212453799c715fff4c76474d8108
Anusha-118 <anushapallem459@gmail.com>
2026-01-03 23:03:07 +0530

    Add files via upload

---
+++ java/Sum_of_N_numbers.java
@@ -0,0 +1,12 @@
+public class Sum_of_N_numbers
+ {
+   public static void main (String[]args)
+   {
+int num=10;
+int sum=0;
+for(int i=0;i<=num;i++){
+  sum+=i;
+  System.out.println(sum);
+}
+   }
+}
