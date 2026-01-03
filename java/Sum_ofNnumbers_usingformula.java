52f578a4f9f4694c0866554663c45fda9f82262c
Anusha-118 <anushapallem459@gmail.com>
2026-01-03 23:03:28 +0530

    Add files via upload

---
+++ java/Sum_ofNnumbers_usingformula.java
@@ -0,0 +1,8 @@
+class Sum_ofNnumbers_usingformula{
+    public static void main(String args[]){
+        int num=7;
+        for(int i=0;i<=num;i++){
+        System.out.println(i*(i+1)/2);
+        }
+    }
+}
