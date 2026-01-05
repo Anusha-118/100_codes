e94dc9d4206179dcfa2fe8f2649aa8eb7ba2b3df
Anusha-118 <anushapallem459@gmail.com>
2026-01-05 21:25:42 +0530

    Add files via upload

---
+++ java/Range.java
@@ -0,0 +1,11 @@
+class Range{
+    public static void main(String args[]){
+        int a=5;
+        int b=10;
+        int sum=0;
+        for(int i=a;i<=b;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
