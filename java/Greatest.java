cc3574b287fbabaa2ec2a0e36e35fa884ca3c3e4
Anusha-118 <anushapallem459@gmail.com>
2026-01-08 21:11:53 +0530

    Add files via upload

---
+++ java/Greatest.java
@@ -0,0 +1,13 @@
+class Greatest{
+    public static void main(String args[]){
+        int a=4;
+        int b=8;
+        if(a<b){
+            System.out.println("a is greater");
+        }
+        else{
+            System.out.println("b is greater");
+        }
+    }
+}
+
