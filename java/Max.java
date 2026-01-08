a8814ff2606b2c483e83404f9617ed7c122e73c5
Anusha-118 <anushapallem459@gmail.com>
2026-01-08 21:11:14 +0530

    Add files via upload

---
+++ java/Max.java
@@ -0,0 +1,12 @@
+class Max{
+    public static void main(String args[]){
+        int num1=2;
+        int num2=7;
+        if(num1==num2){
+            System.out.println("both are equal");
+        }else{
+                    System.out.println(Math.max(num1,num2));
+                }
+    }
+    }
+    
