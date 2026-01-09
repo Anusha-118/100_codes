7ab4a747947ecb4728a3f9cdc2e15204d7a584d2
Anusha-118 <anushapallem459@gmail.com>
2026-01-09 22:47:04 +0530

    Add files via upload

---
+++ java/Greatest_using_if.java
@@ -0,0 +1,17 @@
+public class Greatest_using_if {
+    public static void main(String args[]){
+        int a=500;
+        int b=45;
+        int c=78;
+        if((a>b)&&(a>c)){
+            System.out.println("a is greater");
+        }
+        else if((b>a)&&(b>c)){
+            System.out.println("b is greater");
+        }
+        else{
+            System.out.println("c is greater");
+        }
+    }
+    
+}
