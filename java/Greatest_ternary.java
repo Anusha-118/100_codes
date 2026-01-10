2825369ddb0474482285de2422ac91927b001388
Anusha-118 <anushapallem459@gmail.com>
2026-01-10 12:46:57 +0530

    Add files via upload

---
+++ java/Greatest_ternary.java
@@ -0,0 +1,13 @@
+public class Greatest_ternary {
+    public static void main(String args[]){
+        int a=45;
+        int b=438;
+        int  c=674;
+        int temp,result;
+        temp= a>b ? a : b;
+        result=temp>c ? temp: c;
+        System.out.println(result +" "+"is greater");
+
+    }
+    
+}
