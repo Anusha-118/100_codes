e8899166b0273e7fe19d901018ec5dc79661fa32
Anusha-118 <anushapallem459@gmail.com>
2026-01-10 12:48:28 +0530

    Add files via upload

---
+++ java/Leapyear_boolean.java
@@ -0,0 +1,21 @@
+public class Leapyear_boolean {
+    public static void main(String args[]){
+        int year = 2020;
+        boolean leap;
+
+        if (year % 400 == 0)
+            leap = true;
+
+        else if (year % 4 == 0 && year % 100 != 0)
+            leap = true;
+
+        else
+            leap = false;
+
+        if (leap==true)
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+}
+    
