e49930b86832b36af7a69353108276e669c7d713
Anusha-118 <anushapallem459@gmail.com>
2026-01-25 22:52:57 +0530

    Add files via upload

---
+++ java/Demo.java
@@ -0,0 +1,17 @@
+public class Demo {
+    public static void  display1()
+    {
+        System.out.println("hello java!!!");
+
+    }
+    void display()
+    {
+        System.out.println("Hello javascript!!!");
+    }
+    public static void main(String args[]){
+        Demo d=new Demo();
+        d.display1();
+        
+    }
+    
+}
