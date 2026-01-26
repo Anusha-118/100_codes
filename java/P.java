570700ca7941c205671eb164935b8162158012e7
Anusha-118 <anushapallem459@gmail.com>
2026-01-26 22:20:06 +0530

    Add files via upload

---
+++ java/P.java
@@ -0,0 +1,18 @@
+public class P {
+    public static void main(String[] args) {
+        int n=7;
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                count+=1;
+            }
+        }
+        if(count==0){
+            System.out.println(n+ " "+ "is a prime number");
+        }
+        else{
+            System.out.println(n+" "+ "is not a prime number");
+        }
+    }
+    
+}
