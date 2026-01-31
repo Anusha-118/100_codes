3bb1eaf2fcd7c36e5c07a1d45ad1f944079c9e95
Anusha-118 <anushapallem459@gmail.com>
2026-01-31 22:01:37 +0530

    Add files via upload

---
+++ java/Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+    public static void main(String args[])
+    {
+        int n=10;
+      int num1=0;
+      int num2=1;
+      int num3;
+      
+      System.out.println(num1);
+      System.out.println(num2);
+       for(int i=0;i<=n;i++){
+        num3=num1+num2;
+         num1=num2;
+         num2=num3;
+        System.out.println(num3);
+    }
+}
+}
