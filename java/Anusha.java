63ea95342765ee09d8049b19ec600828f3d597ec
Anusha-118 <anushapallem459@gmail.com>
2026-02-01 22:54:02 +0530

    Add files via upload

---
+++ java/Anusha.java
@@ -0,0 +1,10 @@
+public class Anusha {
+    public static void main(String args[]){
+        midexam();
+    }
+  static void midexam(){
+        System.out.println("repu naku mid exam start.em inka em prepare avvaledhu.andhuke ee code push chesthunna.em anukokuu githubuuu");
+
+    }
+
+}
