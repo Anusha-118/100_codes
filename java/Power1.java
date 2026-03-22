1a98a2de67b537a5594ba1e71e1e271522fdce7a
Anusha-118 <anushapallem459@gmail.com>
2026-03-22 22:28:05 +0530

    Add files via upload

---
+++ java/Power1.java
@@ -0,0 +1,13 @@
+public class Power1 {
+    public static void main(String args[]){
+        int num1=2;
+        int num2=4;
+        int res1=3;
+        int res2=2;
+        int a,b;
+        a=(int) Math.pow(num1,res1);
+        b=(int) Math.pow(num2,res2);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
