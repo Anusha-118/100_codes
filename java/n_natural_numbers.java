fa88086e4f58fb8a083e7a7d31c262b717c2a598
Anusha-118 <anushapallem459@gmail.com>
2026-05-18 22:05:42 +0530

    Add files via upload

---
+++ java/n_natural_numbers.java
@@ -0,0 +1,9 @@
+public class n_natural_numbers {
+    public static void main(String args[]){
+        int n=10, sum=0;
+        for(int i=0; i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
