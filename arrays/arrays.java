15b6e3112248fbcc24829e895cb7c078e5672e11
Anusha-118 <anushapallem459@gmail.com>
2026-05-23 20:40:13 +0530

    Add files via upload

---
+++ arrays/arrays.java
@@ -0,0 +1,10 @@
+class arrays{
+    public static void main(String args[]){
+        int nums[]={3,4,5,6};
+      //  System.out.println(nums[0]);
+      for(int i=0;i<=3;i++){
+         System.out.println(nums[i]);
+
+      }
+    }
+}
